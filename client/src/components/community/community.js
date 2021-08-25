2509c24bc4c119612b5a88b6e4a65c60ab1a5006
Uchechukwu Uboh <uboh@MacBooky-Pro.attlocal.net>
2021-08-25 03:44:36 -0500

    done

---
+++ client/src/components/community/community.js
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+
+export default function Home() {
+  const darkMode = useSelector((state) => state.darkMode);
+  return (
+    <div className="container text-center">
+      <p id={`${darkMode ? "section-title-light" : "section-title-dark"}`}>
+        People motivate me
+      </p>
+      <figure className="text-center">
+        <blockquote className="blockquote">
+          <p
+            id={`${
+              darkMode ? "section-subtitle-light" : "section-subtitle-dark"
+            }`}
+          >
+            "The best way to find yourself is to lose yourself in the service of
+            others."
+          </p>
+        </blockquote>
+        <figcaption
+          id={`${darkMode ? "section-text-light" : "section-text-dark"}`}
+          className="blockquote-footer"
+        >
+          Mahatma Gandhi
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
