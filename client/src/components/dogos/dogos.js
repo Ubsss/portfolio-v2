fabf704afd7e2d30425513ac1591bcdde00d4bea
Uchechukwu Uboh <uboh@MacBooky-Pro.attlocal.net>
2021-08-26 21:44:58 -0500

    completed dogos view

--- client/src/components/dogos/dogos.js
+++ client/src/components/dogos/dogos.js
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
-// import ProfileImage from "./profilePhoto-mini.jpg";
+import Nala from "./nala.jpeg";
+import Sarabi from "./sarabi.jpeg";
 import "./dogos.css";
 
 export default function Home() {
@@ -16,15 +17,45 @@ export default function Home() {
             darkMode ? "section-subtitle-light" : "section-subtitle-dark"
           }`}
         >
-          I'm Uchechukwu Uboh
+          Here are my little trouble makers.
         </span>
       </p>
-      {/* <img
-        id="home-image"
-        className="rounded-circle my-5 img-fluid"
-        src={ProfileImage}
-        alt="home"
-      /> */}
+      <div
+        id={`${darkMode ? "section-text-light" : "section-text-dark"}`}
+        className="row h-100 w-100"
+      >
+        <div className="col-12 col-md-6">
+          {" "}
+          <img
+            id="dogos-image"
+            className="rounded-circle my-2 img-fluid"
+            src={Nala}
+            alt="nala"
+          />
+          <p>
+            <strong>Nala</strong>
+          </p>
+          <p>
+            The responsible older sister that listens sometimes. She loves to
+            play, cuddle and bark at random things.
+          </p>
+        </div>
+        <div className="col-12 col-md-6">
+          <img
+            id="dogos-image"
+            className="rounded-circle my-2 img-fluid"
+            src={Sarabi}
+            alt="sarabi"
+          />
+          <p>
+            <strong>Sarabi</strong>
+          </p>
+          <p>
+            She is the spontaneous younger sister. She loves her belly rubs and
+            arguing with her sister.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
