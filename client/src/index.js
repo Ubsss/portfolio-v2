7bc8a1832cf6ab9ecf9c07b93c1f2f7b9d443ce7
Uchechukwu Uboh <uboh@MacBooky-Pro.local>
2021-09-26 17:37:21 -0500

    fixed app  file name issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./app";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import reducer from "./redux/reducer";
 import { createStore } from "redux";
