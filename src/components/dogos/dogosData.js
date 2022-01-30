181360fa02182b8f0182bf06bed80c4204e1e1df
Uchechukwu Uboh <uboh@MacBooky-Pro.local>
2022-01-30 11:03:15 -0600

    moved dogo images to fb storage

--- src/components/dogos/dogosData.js
+++ src/components/dogos/dogosData.js
@@ -1,22 +1,23 @@
-import nala from "./nala.jpg";
-import nala1 from "./nala1.jpg";
-import nala2 from "./nala2.jpg";
-import sarabi from "./sarabi.jpg";
-import sarabi1 from "./sarabi1.jpg";
-import sarabi2 from "./sarabi2.jpg";
-
 const dogosData = {
   nala: {
     name: "Nala",
     description:
       "The responsible older sister that listens... sometimes. She loves to play, cuddle and bark at random cars.",
-    pictures: [nala, nala1, nala2],
+    pictures: [
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-site-8e4f6.appspot.com/o/dogos%2Fnala3.jpg?alt=media&token=9d7e867a-b7a0-492e-827a-c28ffd6768de",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-site-8e4f6.appspot.com/o/dogos%2Fnala2.jpg?alt=media&token=ecfeedc4-34c6-4622-97f1-5d456ee42b26",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-site-8e4f6.appspot.com/o/dogos%2Fnala1.jpg?alt=media&token=3d6110f4-686b-4d9d-86eb-398b304c74e6",
+    ],
   },
   sarabi: {
     name: "Sarabi",
     description:
       "The spontaneous younger sister. She loves her belly rubs, toys and arguing with her sister.",
-    pictures: [sarabi, sarabi1, sarabi2],
+    pictures: [
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-site-8e4f6.appspot.com/o/dogos%2Fsarabi1.jpg?alt=media&token=f0cbc389-8916-45c6-943e-89d630f7ce35",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-site-8e4f6.appspot.com/o/dogos%2Fsarabi2.jpg?alt=media&token=57d7d7ad-5795-4165-9603-ae11a2ca2af7",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-site-8e4f6.appspot.com/o/dogos%2Fsarabi3.jpg?alt=media&token=45eb05e1-12f4-4c2f-a79e-09baad843c62s",
+    ],
   },
 };
 
