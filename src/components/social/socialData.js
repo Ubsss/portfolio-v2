907912cfca3ea0a365d5a32c47af0bed52fd9f92
Uchechukwu Uboh <uboh@MacBooky-Pro.attlocal.net>
2022-05-23 20:04:32 -0500

    added instagram, twitter, and tiktok

--- src/components/social/socialData.js
+++ src/components/social/socialData.js
@@ -1,13 +1,17 @@
 import linkedIn from "./linkedin.png";
-import gitHub from "./github.png";
+import twitter from "./twitter.png";
+import instagram from "./instagram.png";
+import tiktok from "./tik-tok.png";
 
 const socialData = [
+  { name: "Tik-Tok", link: "https://www.tiktok.com/@ucheeuboh", icon: tiktok },
+  { name: "Instagram", link: "https://www.instagram.com/ucheeuboh", icon: instagram },
+  { name: "Twitter", link: "https://twitter.com/ucheeuboh", icon: twitter },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/uchechukwu-uboh-31030196/",
+    link: "https://www.linkedin.com/in/ucheeuboh",
     icon: linkedIn,
   },
-  { name: "GitHub", link: "https://github.com/Ubsss", icon: gitHub },
 ];
 
 export { socialData };
