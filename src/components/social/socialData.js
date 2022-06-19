f96a4fa328466991ce478733cd9fee2ba622e6e0
Uchechukwu Uboh <uboh@MacBooky-Pro.attlocal.net>
2022-06-19 00:20:01 -0500

    added youtube channel to social

--- src/components/social/socialData.js
+++ src/components/social/socialData.js
@@ -2,10 +2,20 @@ import linkedIn from "./linkedin.png";
 import twitter from "./twitter.png";
 import instagram from "./instagram.png";
 import tiktok from "./tik-tok.png";
+import youtube from "./youtube.png";
 
 const socialData = [
   { name: "Tik-Tok", link: "https://www.tiktok.com/@ucheeuboh", icon: tiktok },
-  { name: "Instagram", link: "https://www.instagram.com/ucheeuboh", icon: instagram },
+  {
+    name: "Youtube",
+    link: "https://www.youtube.com/channel/UC_gm8IUjrt4riVrBEDX2PKw",
+    icon: youtube,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/ucheeuboh",
+    icon: instagram,
+  },
   { name: "Twitter", link: "https://twitter.com/ucheeuboh", icon: twitter },
   {
     name: "LinkedIn",
